44a5b95d7c5d61293d15dd13177b018ea3b37f9a
JontyDutta <jonty.duttanspa@gmail.com>
2026-07-08 22:18:47 +0530

    Fix Express trust proxy rate limit bug

--- server.js
+++ server.js
@@ -19,9 +19,11 @@ app.use(helmet({
 
 app.use(compression());
 
+app.set('trust proxy', 1);
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 1000,
   message: 'Too many requests from this IP, please try again later.'
 });
 app.use(limiter);
